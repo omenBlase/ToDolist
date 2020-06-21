eb8f36fb88572d15acdb6befc03051b643c06abd
Błażej <b-wojtusciszyn@wp.pl>
2020-06-21 20:33:44 +0200

    some changes

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const btn = document.querySelector('button');
-const txtAdd = document.querySelector('input');
-const element = document.createElement('addto');
-
+const txtAdd = document.getElementById('lname');
+const element = document.querySelector('addto');
 const addElement = function() {
 	console.log('klik');
-	document.body.appendChild('element');
+
 	// const div = document.create('div');
-	element.textContent = txtAdd;
+	// element.textContent = txtAdd;
 	element.style.display = 'block';
 };
 btn.addEventListener('click', addElement);
